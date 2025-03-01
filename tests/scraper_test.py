edc15c7315733bf26927078095671f0b885a876f
Michael Lobman <michael.lobman@gmail.com>
2025-03-01 17:56:12 -0500

    clean up test

--- tests/scraper_test.py
+++ tests/scraper_test.py
@@ -1,8 +1,9 @@
 import unittest
-from scraper import scraper
+from scraper.scraper import Scraper
 
 class TestScraper(unittest.TestCase):
-	def test_instatiation():
+	def test_instatiation(self):
+		print("testing test_instatiation")
 		test_object = Scraper(False)
 		self.assertEqual(test_object.last_workout, '02.28.2025 "VESTED DT"')
 
