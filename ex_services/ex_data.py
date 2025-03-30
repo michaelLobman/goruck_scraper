24a542d2e5c5637ca5030826940bc42d0c0b0455
Michael Lobman <michael.lobman@gmail.com>
2025-03-30 09:49:22 -0400

    simplifiy one-line if statement

--- ex_services/ex_data.py
+++ ex_services/ex_data.py
@@ -2,6 +2,6 @@ from ex_services.regex_utils import RegexUtils
 
 class ExData():
 	def __init__(self, ex=None, ex_reps=None, has_alt=False):
-		self.ex = ex if ex else RegexUtils.try_match(ex_reps, "ex")
+		self.ex = ex or RegexUtils.try_match(ex_reps, "ex")
 		self.reps = None if ex else RegexUtils.try_match(ex_reps, "reps")
 		self.has_alt = has_alt
