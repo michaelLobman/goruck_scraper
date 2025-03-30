ce7b060146da22908262dc95bf77be9daaf375ad
Michael Lobman <michael.lobman@gmail.com>
2025-03-30 08:30:52 -0400

    refactor converter to separate methods, include check_df method that prints ex/reps for each title

--- converter/converter.py
+++ converter/converter.py
@@ -7,8 +7,14 @@ from csv_services.ex_dataset import ExDataset
 class Converter():
 	def __init__(self):
 		self.ex_list = []
+		self._df = None
+		self._check_cols = ['ex', 'reps']
 
 	def execute(self):
+		self.initialize_df()
+		self.check_df()
+		
+	def initialize_df(self):
 		with open("./test_files/convert_test.txt", "r") as txt:
 			ex = ExDataset()
 			for line in txt:
@@ -19,6 +25,11 @@ class Converter():
 			if not any(ex._metadata.title == x.title for x in self.ex_list):
 				ex.compile_data()
 				self.ex_list.extend(ex.data)
-		df = pd.DataFrame([vars(ex_data) for ex_data in self.ex_list])
-		print(df)
-		# write to file
+		self._df = pd.DataFrame([vars(ex_data) for ex_data in self.ex_list])
+
+	def check_df(self):
+		unique_titles = self._df['title'].unique()
+		for title in unique_titles:
+			print(f"___{title}___")
+			filtered_by_title = self._df.loc[self._df['title'] == title, self._check_cols]
+			print(filtered_by_title)
